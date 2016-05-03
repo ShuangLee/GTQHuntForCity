90eb6487c263469a569b3f328dabe13bea2786d1
LShawn <730356947@qq.com>
2016-05-03 16:01:15 +0800

    Initial Commit

---
+++ GTQHuntForCity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GTQHuntForCity
+//
+//  Created by Ls on 16/5/3.
+//  Copyright © 2016年 Ls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
